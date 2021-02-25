bb9d3aa5312f8be0ea440314e1594411724d0ee7
Neeraj <neeraj.s1988@gmail.com>
2021-02-25 15:33:10 +0530

    updated default route path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CreateInvoiceComponent } from './pages/create-invoice/create-invoice.co
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent},
   { path: 'create-invoice', component: CreateInvoiceComponent },
 ];
